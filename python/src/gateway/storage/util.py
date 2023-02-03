77bc3e555fae3f98de443f4d02428a7cf82c32ad
Nischal Shakya <nischalshakya2015@gmail.com>
2023-02-03 16:51:19 +0545

    wip: implement gateway server along with RabbitMQ
    The logic for the gateway server is implemented in the gateway/server.py
    file.
    
    The upload logic is underway, but the download logic is not yet
    implemented

---
+++ python/src/gateway/storage/util.py
@@ -0,0 +1,22 @@
+import pika, json
+
+def upload(f, fs, channel, access):
+  try: 
+    fid = fs.put(f)
+  except Exception as err:
+    return "internal server error", 500\
+
+  message = {
+    "video_fid": str(fid),
+    "mp3_fid": None,
+    "username": access["username"]
+  }
+
+  try:
+    channel.basic_publish(
+      exchange="",
+
+    )
+
+  except Exception as err:
+    return "internal server error", 500
